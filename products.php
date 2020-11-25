d83c232a69a8d2a89295a40d2d9921ba93af19c5
Noura <75036072+NOURAYM9@users.noreply.github.com>
2020-11-25 20:47:44 +0300

    Add files via upload

---
+++ products.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="navbar" style="background-color:black;">
+        <div class="container">
+            <div class="conn">
+                <div class="log">
+                    <a href="index.php"><img src="./images/1.png" style="margin-top: -16px;height: 95px;"></a>
+                </div>
+                <div class="aaa">
+                    <a href="products.php" style="text-decoration: none;font-size: 65px;color: white;">Abaya & More</a>
+                </div>                
+            </div> 
+        </div>
+    </header>
+    
+
+    <section class="products">
+        <div class="wholeAbaya">
+            <div class="singleAbaya">
+                <div class="abayaImg">
+                <img src="./images/abaya1.webp" alt="">
+                </div>
+                <h3>عبايه سوداء</h3>
+            </div>
+            <div class="singleAbaya">
+                <div class="abayaImg">
+                <img src="./images/abaya2.jpg" alt="">
+                </div>
+                <h3>عبايه سوداء مع بني</h3>
+            </div>
+            <div class="singleAbaya">
+                <div class="abayaImg">
+                <img src="./images/ANYFR-4-scaled.jpg" alt="">
+                </div>
+                <h3>عبايه سوداء مع وردي</h3>
+            </div>
+            <div class="singleAbaya">
+                <div class="abayaImg">
+                <img src="./images/abaya1.webp" alt="">
+                </div>
+                <h3>عبايه سوداء</h3>
+            </div>
+            <div class="singleAbaya">
+                <div class="abayaImg">
+                <img src="./images/abaya2.jpg" alt="">
+                </div>
+                <h3>عبايه سوداء مع بني</h3>
+            </div>
+            <div class="singleAbaya">
+                <div class="abayaImg">
+                <img src="./images/ANYFR-4-scaled.jpg" alt="">
+                </div>
+                <h3>عبايه سوداء مع وردي</h3>
+            </div>
+        </div>
+    </section>
+
+    <section class="products">
+        <div class="wholeDetails">
+            <div class="notDetails">
+                <img style="width: 50px; padding-top: 1em; padding-bottom:0.5em;" src="./images/headphones.png" alt="">
+                <h3>خدمة العملاء
+                </h3>
+                <div class="underContact">
+<p>متوفرين لخدمتكم على مدار الساعة 24 ساعة
+
+</p>
+                </div>
+            </div>
+            <div class="notDetails">
+                <img style="width: 50px; padding-top: 1em; padding-bottom:0.5em;" src="./images/money.png" alt="">
+                <h3>طرق الدفع
+                </h3>
+                <div class="underContact">
+<p>متنوعة وتلبي احتياجاتك ( مدى ، فيزا ، تحويل بنكي )
+
+</p>
+                </div>
+            </div>
+            <div class="notDetails">
+                <img style="width: 50px; padding-top: 1em; padding-bottom:0.5em;" src="./images/car.png" alt="">
+                <h3>التوصيل فائق السرعة
+                </h3>
+                <div class="underContact">
+<p>طرق توصيل متنوعة وسريعة لجميع مناطق المملكة
+
+</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="homePay">
+        <img src="./images/cc.webp" alt="">
+        <img src="./images/applepay.svg" alt="">
+</div>
+    
+<footer dir="rtl">
+    <div class="contact">
+        <h2>من نحن
+        </h2>
+        <ul>
+            <li>متجر مختص بالعبايات الجاهزة و التفصيل</li>
+        </ul>
+    </div>
+    <div class="contact">
+        <h2>روابط مهمة
+
+        </h2>
+        <ul>
+            <li>تواصل معنا</li>
+            <li>التوصيل والشحن</li>
+            <li>سياسة الاسترجاع والاستبدال</li>
+        </ul>
+    </div>
+    <div class="contact">
+        <h2>تابعنا على الشبكات الاجتماعية
+
+        </h2>
+        <ul>
+            <li><img style="width: 40px;" src="./images/instagram.png" alt=""></li>
+        </ul>
+    </div>
+</footer>
+
+</body>
+</html>
