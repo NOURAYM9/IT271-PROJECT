e30f8325c28f25b3c90cc2ca6a488127464a426e
ReemAlhassoun <75035922+ReemAlhassoun@users.noreply.github.com>
2020-11-26 23:04:10 +0300

    Update products.php

--- products.php
+++ products.php
@@ -13,8 +13,8 @@
                 <div class="log">
                     <a href="index.php"><img src="./images/1.png" style="margin-top: -16px;height: 95px;"></a>
                 </div>
-                <div class="aaa">
-                    <a href="products.php" style="text-decoration: none;font-size: 65px;color: white;">Abaya & More</a>
+                <div>
+                    <a href="products.php" style="text-decoration: none;font-size: 65px;color: white;">Abayas& More</a>
                 </div>                
             </div> 
         </div>
