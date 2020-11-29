b0a5563b86280ac86ef64df3d2f30d76fd504bee
ReemAlhassoun <75035922+ReemAlhassoun@users.noreply.github.com>
2020-11-29 16:32:33 +0300

    Update abaya.sql

--- abaya.sql
+++ abaya.sql
@@ -1,28 +1,21 @@
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
--- Database: `abaya`
-
+-- بنية الجدول `users`
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `pass` varchar(50) NOT NULL
-) --ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
---import table from phpmyadmin
 
 INSERT INTO `users` (`id`, `email`, `pass`) VALUES
 (1, 'sara@gmail.com', '123456'),
 (2, 'saraali@gmail.com', '56577878');
 
-
--- Indexes for table `users`
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
@@ -31,8 +24,8 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 
 
-
 -- AUTO_INCREMENT for table `users`
 
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT; -- AUTO_INCREMENT=3;
+COMMIT;
